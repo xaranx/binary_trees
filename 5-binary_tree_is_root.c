b5b22feb516d1b6a44f04ea0d1d0418bdb7d6ea6
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-11-01 15:11:36 -0700

    task 3, 4, 5, 6

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a node is root
+ * @node: pointer to the node to check
+ * Return: 1 if node is root, otherwise 0
+*/
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	return (node != NULL && !node->parent);
+}
