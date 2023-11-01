ae0500cc02739219874e8521af60b00288b1078c
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-11-01 13:48:09 -0700

    update 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -25,10 +25,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->left = parent->left;
+	new_node->left = NULL;
 	new_node->right = NULL;
 	if (parent->left != NULL)
 	{
+		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
