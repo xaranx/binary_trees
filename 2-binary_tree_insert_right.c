b3033482c93566493348d75a72352467ec1a5b17
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-11-01 14:25:31 -0700

    update task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,21 +12,31 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_node = NULL;
 
+	if (parent == NULL)
+		return (NULL);
+
+	/*memory allocation*/
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
+
+	/*initialize right node*/
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->right = parent->right;
+	new_node->right = NULL;
 	new_node->left = NULL;
+
+	/*if parent has node*/
 	if (parent->right != NULL)
 	{
-		parent->right->parent = new_node;
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
 	}
+	/*set new node as child*/
 	parent->right = new_node;
+
+	/*return new node*/
 	return (new_node);
 }
